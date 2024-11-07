be389d4a9a147eaad47b18fc79c9be7b6102399f
Joel Eckroth <joel.eckroth@gmail.com>
2024-11-07 14:56:01 -0700

    label updates

--- src/components/SvgColorController.jsx
+++ src/components/SvgColorController.jsx
@@ -234,7 +234,7 @@ const SvgColorController = () => {
                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
             }`}
           >
-            Default Icons
+            New Icon Set
           </button>
           <button
             onClick={() => setActiveTab('new')}
@@ -244,7 +244,7 @@ const SvgColorController = () => {
                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
             }`}
           >
-            New Icons
+            Existing Icons
           </button>
         </nav>
       </div>
